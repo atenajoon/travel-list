925052d4ec201f4263b84f77bb38237b0b6487db
Atena Razm <atenarazm@gmail.com>
2024-06-18 12:37:29 -0700

    form input change and submit handlerd implemented

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const initialItems = [
   { id: 1, description: 'Passports', quantity: 2, packed: false },
   { id: 2, description: 'Socks', quantity: 12, packed: true },
@@ -20,8 +22,15 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState('');
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(description);
+    setDescription('');
+  }
+
   return (
-    <div className='add-form'>
+    <form className='add-form' onSubmit={handleSubmit}>
       <h3> What do you need for your 😎 trip?</h3>
       <select>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
@@ -30,9 +39,14 @@ function Form() {
           </option>
         ))}
       </select>
-      <input type='text' placeholder='Item...' />
+      <input
+        onChange={(e) => setDescription(e.target.value)}
+        type='text'
+        placeholder='Item...'
+        value={description}
+      />
       <button>Add</button>
-    </div>
+    </form>
   );
 }
 
